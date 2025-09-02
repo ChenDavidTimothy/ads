656e0d6249eba834511abe1f8c491eaa8d7bee9d
ChenDavidTimothy <chendavidtimothy@gmail.com>
2025-09-02 16:35:08 +0200

    logo

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -2,261 +2,128 @@ import * as React from "react";
 
 const SVGComponent = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
-    xmlns="http://www.w3.org/2000/svg"
-    xmlnsXlink="http://www.w3.org/1999/xlink"
     width={200}
     height={50}
     viewBox="0 0 200 50"
     preserveAspectRatio="xMidYMid"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
     {...props}
   >
     <defs>
       <linearGradient
-        id="bgGradient"
-        x1={0}
-        y1={0}
-        x2={360}
-        y2={360}
+        id="a"
+        x1={18.105}
+        y1={38.263}
+        x2={162.166}
+        y2={182.324}
+        gradientTransform="matrix(.29073 0 0 .25648 -4.185 -3.468)"
         gradientUnits="userSpaceOnUse"
-        gradientTransform="matrix(0.55555556,0,0,0.13888889,0,0)"
       >
         <stop
           offset="0%"
           style={{
-            stopColor: "#000000",
+            stopColor: "#a78bfa",
+            stopOpacity: 1,
+          }}
+        />
+        <stop
+          offset="50%"
+          style={{
+            stopColor: "#8b5cf6",
             stopOpacity: 1,
           }}
         />
         <stop
           offset="100%"
           style={{
-            stopColor: "#0a0a0a",
+            stopColor: "#6366f1",
             stopOpacity: 1,
           }}
         />
       </linearGradient>
       <linearGradient
-        id="blueGradient"
-        x1={12.187647}
-        y1={49.803329}
-        x2={27.154278}
-        y2={49.803329}
+        id="b"
+        x1={15.569}
+        y1={86.665}
+        x2={112.751}
+        y2={183.846}
+        gradientTransform="matrix(.48273 0 0 .15446 -4.185 -3.468)"
         gradientUnits="userSpaceOnUse"
-        gradientTransform="matrix(1.0022297,0,0,0.33407654,31.275997,-73.528645)"
       >
         <stop
           offset="0%"
-          style={{
-            stopColor: "#1e3a8a",
-            stopOpacity: 1,
-          }}
-        />
-        <stop
-          offset="100%"
-          style={{
-            stopColor: "#2563eb",
-            stopOpacity: 1,
-          }}
-        />
-      </linearGradient>
-      <linearGradient
-        id="purpleGradient"
-        x1={12.187647}
-        y1={87.219902}
-        x2={27.154278}
-        y2={87.219902}
-        gradientUnits="userSpaceOnUse"
-        gradientTransform="matrix(1.0022296,0,0,0.33407654,31.275997,-73.419267)"
-      >
-        <stop
-          offset="0%"
-          style={{
-            stopColor: "#581c87",
-            stopOpacity: 1,
-          }}
-        />
-        <stop
-          offset="100%"
           style={{
             stopColor: "#7c3aed",
             stopOpacity: 1,
           }}
         />
-      </linearGradient>
-      <linearGradient
-        id="greyGradient"
-        x1={17.553185}
-        y1={68.511612}
-        x2={32.519814}
-        y2={68.511612}
-        gradientUnits="userSpaceOnUse"
-        gradientTransform="matrix(1.0022296,0,0,0.33407654,32.227144,-73.473956)"
-      >
-        <stop
-          offset="0%"
-          style={{
-            stopColor: "#4b5563",
-            stopOpacity: 1,
-          }}
-        />
         <stop
           offset="100%"
           style={{
-            stopColor: "#6b7280",
+            stopColor: "#6d28d9",
             stopOpacity: 1,
           }}
         />
       </linearGradient>
-      <linearGradient
-        id="blueBar"
-        xlinkHref="#blueGradient"
-        gradientUnits="userSpaceOnUse"
-        gradientTransform="matrix(1.0022297,0,0,0.33407654,25.771083,-74.239009)"
-        x1={12.187647}
-        y1={49.803329}
-        x2={27.154278}
-        y2={49.803329}
-      />
-      <linearGradient
-        id="purpleBar"
-        xlinkHref="#purpleGradient"
-        gradientUnits="userSpaceOnUse"
-        gradientTransform="matrix(1.0022296,0,0,0.33407654,25.771083,-75.071516)"
-        x1={12.187647}
-        y1={87.219902}
-        x2={27.154278}
-        y2={87.219902}
-      />
-      <linearGradient
-        id="greyBar"
-        xlinkHref="#greyGradient"
-        gradientUnits="userSpaceOnUse"
-        gradientTransform="matrix(1.0022296,0,0,0.33407654,24.559913,-74.655257)"
-        x1={17.553185}
-        y1={68.511612}
-        x2={32.519814}
-        y2={68.511612}
-      />
     </defs>
-    <g transform="matrix(1.1670838,0,0,1.1670838,-33.470205,-4.5661047)">
-      <g transform="matrix(1.455019,0,0,1.4526924,-26.591717,96.903311)">
-        <g transform="matrix(-0.42031259,0,0,0.31523443,70.985239,-95.589706)">
-          <rect
-            x={36.87656}
-            y={152.49754}
-            width={1.9826514}
-            height={13.217676}
-            rx={0.06245352}
-            style={{
-              fill: "#f8fafc",
-              fillOpacity: 1,
-              strokeWidth: 0.31995,
-            }}
-          />
-        </g>
-        <g transform="matrix(0,0.42031259,0.63046887,0,-48.992716,-59.349738)">
-          <rect
-            x={36.87656}
-            y={152.49754}
-            width={1.9826514}
-            height={13.217676}
-            rx={0.06245352}
-            style={{
-              fill: "#f8fafc",
-              fillOpacity: 1,
-              strokeWidth: 0.31995,
-            }}
-          />
-        </g>
-        <g transform="matrix(0.42031259,0,0,0.31523443,24.152889,-101.54527)">
-          <rect
-            x={36.87656}
-            y={152.49754}
-            width={1.9826514}
-            height={13.217676}
-            rx={0.06245352}
-            style={{
-              fill: "#f8fafc",
-              fillOpacity: 1,
-              strokeWidth: 0.31995,
-            }}
-          />
-        </g>
-        <g transform="matrix(0,0.42031259,-0.63046887,0,144.13086,-65.183486)">
-          <rect
-            x={36.87656}
-            y={152.49754}
-            width={1.9826514}
-            height={13.217676}
-            rx={0.06245352}
-            style={{
-              fill: "#f8fafc",
-              fillOpacity: 1,
-              strokeWidth: 0.31995,
-            }}
-          />
-        </g>
-        <rect
-          x={37.985905}
-          y={-57.600887}
-          width={15}
-          height={5}
-          rx={0.053571429}
-          fill="url(#blueBar)"
-          style={{
-            strokeWidth: 0.541263,
-          }}
-        />
-        <rect
-          x={37.985905}
-          y={-45.933388}
-          width={15}
-          height={5}
-          rx={0.053571425}
-          fill="url(#purpleBar)"
-          style={{
-            strokeWidth: 0.541263,
-          }}
-        />
-        <rect
-          x={42.152225}
-          y={-51.767136}
-          width={15}
-          height={5}
-          rx={0.053571425}
-          fill="url(#greyBar)"
-          style={{
-            strokeWidth: 0.541267,
-          }}
-        />
-      </g>
-      <g transform="matrix(2.5150884,0,0,2.5150884,-30.353914,-100.71914)">
-        <text
-          x={35.564266}
-          y={54.848934}
-          style={{
-            fontFamily: "Montserrat",
-            fontWeight: 600,
-            fontSize: "12.24px",
-            fill: "#f8fafc",
-            fillOpacity: 1,
-            strokeWidth: 0.421966,
-          }}
-        >
-          Variota
-        </text>
-        <circle
-          cx={74.482109}
-          cy={46.313694}
-          r={1.0113109}
-          style={{
-            fill: "#7c3aed",
-            fillOpacity: 1,
-            strokeWidth: 0.288738,
-          }}
-        />
-      </g>
-    </g>
+    <text
+      x={54.552}
+      y={40.687}
+      style={{
+        fontFamily: "Montserrat",
+        fontWeight: 600,
+        fontSize: "39.7773px",
+        fill: "#e2e8f0",
+        fillOpacity: 1,
+        strokeWidth: 1.37129,
+      }}
+    >
+      Variota
+    </text>
+    <circle
+      cx={127.698}
+      cy={12.373}
+      r={5.191}
+      style={{
+        fill: "#7c3aed",
+        fillOpacity: 1,
+        strokeWidth: 1.482,
+      }}
+    />
+    <path
+      style={{
+        fontVariationSettings: "\"wght\" 600",
+        fill: "url(#a)",
+        stroke: "none",
+        strokeWidth: 0.469398,
+        strokeDasharray: "none",
+        strokeOpacity: 1,
+      }}
+      d="m7.687 18.236 4.499-.026c.991-.003 2.49.623 3.159 2.139l8.757 17.881c.879 1.264 2.159 2.016 3.982 2.076l4.665.043c-1.973-.605-3.746-2.86-4.47-4.904-3.776-7.626-6.465-13.077-9.698-19.615-.745-1.249-1.547-2.458-3.02-2.635l-4.04-.052 1.637 2.711-3.234-.088-3.098-5.46-1.472-.01V7.29l2.918.009.834 1.386.95 1.568 5.667.03c2.895.291 4.093 1.953 5.228 3.665l10.406 21.447c1.96 2.884 3.654 3.25 5.154 1.783l2.394-4.91 4.055-2.564-4.983 10.626c-1.786 2.665-3.59 3.022-5.399 2.955l-5.72-.008c-2.058-.04-4.38-1.9-5.394-3.705L12.566 21.46c-.72-1.327-1.876-1.394-3.005-1.537-1.644-.239-1.52-1.038-1.874-1.686M1.078 6.346h3.023v3.022H1.078Z"
+    />
+    <path
+      style={{
+        fontVariationSettings: "\"wght\" 600",
+        fill: "url(#b)",
+        stroke: "none",
+        strokeWidth: 0.469398,
+        strokeDasharray: "none",
+        strokeOpacity: 1,
+      }}
+      d="M3.33 11.61h3.023v3.023H3.33Zm24.143 10.583 1.404 2.736 5.332-10.714c.586-1.216.975-1.177 2.107-1.18l8.91-.02c.936.066 2.488-.226 1.912 1.88l-2.168 4.992 2.177 1.797 2.763-5.977c1.327-3.839-1.536-5.838-4.58-5.777l-10.456-.012c-1.36.09-2.387 1.127-3.145 3.023z"
+    />
+    <path
+      style={{
+        fontVariationSettings: "\"wght\" 600",
+        fill: "#e2e8f0",
+        stroke: "none",
+        strokeWidth: 0.469398,
+        strokeDasharray: "none",
+        strokeOpacity: 1,
+      }}
+      d="M37.241 19.8c-1.532-1.67-.179-2.854 1.365-2.14l6.39 4.628c1.807 1.215 1.862 2.43-.016 3.645l-6.53 4.508c-1.87 1-2.489-1.676-1.347-2.21l5.942-4.146z"
+    />
   </svg>
 );
 
